3ff71142d427dfe3bf7820d13dbbdbe89bf9ac68
Amanda Mkhize <amandamkhize@yahoo.com>
2023-10-05 15:48:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *  * Description: starting point
- *  * Return: 0
-*/
+ * main - the print function
+ * Return: 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
