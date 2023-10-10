e80b061315a765c77426f4e2c54ed76c2d432ce1
Amanda Mkhize <amandamkhize@yahoo.com>
2023-10-10 09:32:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
-	char ch = 'A';
-
-	printf("value of %d is %d\n", ch, ch);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
