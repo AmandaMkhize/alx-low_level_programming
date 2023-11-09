89e1d90d22b30e59c6f3bda9c3423600b671ec76
Amanda Mkhize <amandamkhize@yahoo.com>
2023-11-09 12:47:40 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,10 +21,10 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
+					printf("%s%c", sep, va_arg(list, unsigned int));
 					break;
 				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
+					printf("%s%d", sep, va_arg(list, unsigned int));
 					break;
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
