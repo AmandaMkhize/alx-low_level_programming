fe030d0111ca389aba814d7c037ebbf80d315cbb
Amanda Mkhize <amandamkhize@yahoo.com>
2023-11-16 15:26:45 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * custom_init - prints a custom message before the main
+ * function is executed
+ */
+
+void custom_init(void)
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
