d68505ef69e927ae13f4ecef444707655fc1df4b
Amanda Mkhize <amandamkhize@yahoo.com>
2023-11-16 13:33:16 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	for (; h; h = h->next, s++)
+	 while (h)
 {
-	if (!h->str)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
+	 if (!h->str)
+		 printf("[0] (nil)\n");
+	 else
+		 printf("[%u] %s\n", h->len, h->str);
+	 h = h->next;
+	 s++;
 }
 	return (s);
 }
+
