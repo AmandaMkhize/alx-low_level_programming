d57db9d6b82fffde4c08724b859d733dab25ca27
Amanda Mkhize <amandamkhize@yahoo.com>
2023-11-27 12:52:54 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,44 +9,28 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (!filename)
-return (0);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-int fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-perror("Error opening file");
-return (0);
-}
+	if (!filename)
+		return (0);
 
-char *buf = malloc(letters);
-if (!buf)
-{
-perror("Error allocating memory");
-close(fd);
-return (0);
-}
+	fd = open(filename, O_RDONLY);
 
-ssize_t nrd, nwr;
-while ((nrd = read(fd, buf, letters)) > 0)
-{
-nwr = write(STDOUT_FILENO, buf, nrd);
-if (nwr == -1)
-{
-perror("Error writing to STDOUT");
-close(fd);
-free(buf);
-return (0);
-}
-}
-if (nrd == -1)
-{
-perror("Error reading from file");
-close(fd);
-free(buf);
-return (0);
-}
-close(fd);
-free(buf);
-return (nwr);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
